804d9b5399f48444bfe91e8a7e7d39186de5a77a
stardvst <lilia.manukyan@hotmail.com>
2025-05-28 19:47:36 +0400

    template method NVI example

---
+++ ch14-template-method-and-nvi/8-template-method-nvi/template-method-nvi.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+class Base
+{
+public:
+  bool TheAlgorithm()
+  {
+    if (!Step1())
+    {
+      std::cout << "Step1 failed." << std::endl;
+      return false;
+    }
+    Step2();
+    return true;
+  }
+
+  virtual ~Base() = default;
+
+private:
+  virtual bool Step1()
+  {
+    std::cout << "Base Step1 executed." << std::endl;
+    return true;
+  }
+
+  virtual void Step2() = 0;
+};
+
+class Derived1 : public Base
+{
+  void Step2() override
+  {
+    std::cout << "Derived1 Step2 executed." << std::endl;
+  }
+};
+
+class Derived2 : public Base
+{
+  bool Step1() override
+  {
+    std::cout << "Derived2 Step1 executed." << std::endl;
+    return false;
+  }
+
+  void Step2() override
+  {
+    std::cout << "Derived2 Step2 executed." << std::endl;
+  }
+};
+
+class Derived3 : public Base
+{
+  bool Step1() override
+  {
+    std::cout << "Derived3 Step1 executed." << std::endl;
+    return true;
+  }
+
+  void Step2() override
+  {
+    std::cout << "Derived3 Step2 executed." << std::endl;
+  }
+};
+
+int main()
+{
+  auto b1 = std::make_unique<Derived1>();
+  b1->TheAlgorithm();
+  std::cout << "------------------------" << std::endl;
+
+  auto b2 = std::make_unique<Derived2>();
+  b2->TheAlgorithm();
+  std::cout << "------------------------" << std::endl;
+
+  auto b3 = std::make_unique<Derived3>();
+  b3->TheAlgorithm();
+
+  return 0;
+}
